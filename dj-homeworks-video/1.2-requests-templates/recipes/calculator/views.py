1de27f39d48718344dd261c06d7519222fb35764
Codco <108819690+Codco@users.noreply.github.com>
2022-10-12 15:14:54 +0300

    Update views.py

--- dj-homeworks-video/1.2-requests-templates/recipes/calculator/views.py
+++ dj-homeworks-video/1.2-requests-templates/recipes/calculator/views.py
@@ -19,6 +19,19 @@ DATA = {
     # можете добавить свои рецепты ;)
 }
 
+def calculator(request, recipe):
+    servings = int(request.GET.get('servings', 1))
+    recipe = DATA.get(recipe).copy()
+    for key, value in recipe.items():
+        recipe[key] = value * servings
+    context = {
+        'recipe': recipe,
+        'servings': servings
+
+    }
+    return render(request, 'calculator/index.html', context)
+
+
 # Напишите ваш обработчик. Используйте DATA как источник данных
 # Результат - render(request, 'calculator/index.html', context)
 # В качестве контекста должен быть передан словарь с рецептом:
