59ad724937cbfd6955b0d9fe1a71fb2adc455c3d
Codco <108819690+Codco@users.noreply.github.com>
2022-10-15 09:06:04 +0300

    Update models.py

--- dj-homeworks-video/2.1-databases/work_with_database/phones/models.py
+++ dj-homeworks-video/2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,9 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=40)
+    image = models.CharField(max_length=200)
+    price = models.IntegerField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField(default=False)
+    slug = models.SlugField(unique=True)
