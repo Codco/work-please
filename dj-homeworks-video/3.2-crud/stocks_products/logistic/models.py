385789b75509de524b8d6ee9d7063aa8eecc6924
Codco <108819690+Codco@users.noreply.github.com>
2022-10-23 11:25:31 +0300

    Update models.py

--- dj-homeworks-video/3.2-crud/stocks_products/logistic/models.py
+++ dj-homeworks-video/3.2-crud/stocks_products/logistic/models.py
@@ -3,20 +3,30 @@ from django.db import models
 
 
 class Product(models.Model):
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=60, unique=True)
     description = models.TextField(null=True, blank=True)
 
+    class Meta:
+        ordering = ['title']
 
 class Stock(models.Model):
+    id = models.AutoField(primary_key=True)
     address = models.CharField(max_length=200, unique=True)
     products = models.ManyToManyField(
         Product,
         through='StockProduct',
         related_name='stocks',
     )
+    
+    class Meta:
+        ordering = ['id']
 
 
 class StockProduct(models.Model):
+    class Meta:
+        unique_together = (('stock', 'product'),)
+
     stock = models.ForeignKey(
         Stock,
         on_delete=models.CASCADE,
